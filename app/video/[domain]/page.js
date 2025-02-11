2c8245a2b3855c323fb75faec91e8e03cf62a170
Rodolfo Carvalho <rhcarvalho@gmail.com>
2025-02-11 01:03:03 +0100

    Give each VideoHub component a unique key prop
    This forces React to re-render the component when the key changes, which
    is necessary when navigating between videos on the same page.
    
    See https://react.dev/learn/preserving-and-resetting-state#option-2-resetting-state-with-a-key

--- app/video/[domain]/page.js
+++ app/video/[domain]/page.js
@@ -17,7 +17,7 @@ export default async function Page({ params, searchParams }) {
       <Link href="/" className="flex items-center gap-1 text-blue-500"><ArrowLeftCircleIcon className="size-6" /> Back to home</Link>
       <h2 className="text-2xl font-semibold text-center sm:text-left mt-4">Video: <span className={blur ? "blur" : ""}>{id}</span></h2>
       <p className="text-sm italic text-neutral-700 text-center sm:text-left">Some text BEFORE video player component</p>
-      <VideoHub domain={domain} id={id} />
+      <VideoHub key={id} domain={domain} id={id} />
       <p className="text-sm italic text-neutral-700 text-center sm:text-left">Some text AFTER video player component</p>
       <div className="flex flex-col gap-2">
         <Other domain={domain} id={id} query={await searchParams} />
